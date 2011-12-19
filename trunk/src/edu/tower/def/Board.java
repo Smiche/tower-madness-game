aef20b3847e22c1cbb7fd2b78380df4502982e16
smichess@gmail.com <smichess@gmail.com@a431fb20-beb4-5997-3188-ae3f4862c6d2>
2011-12-19 19:49:06 +0000

    walkMonster() method was causing laggs.

---
+++ trunk/src/edu/tower/def/Board.java
@@ -0,0 +1,28 @@
+package edu.tower.def;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+
+import javax.swing.*;
+
+public class Board extends JPanel{
+	Image img1;
+	public Board(){
+		setFocusable(true);
+		ImageIcon i = new ImageIcon("C:/test.jpg");
+		img1 = i.getImage();
+	}
+	
+	
+
+	public void paint(Graphics g){
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(img1, 0,0,null);
+		g2d.drawImage(Main.monsters.getImage(),(int) Main.monsters.getX(),(int) Main.monsters.getY(), null);
+		Main.sleep(100);
+		repaint();
+	}
+}
